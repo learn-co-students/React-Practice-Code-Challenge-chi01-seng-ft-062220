eb0508897419973c8c66c38b9adf6b89cfe9ade5
J-Waz <jgw.jakegeorge@gmail.com>
2020-08-28 10:09:08 -0500

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,20 +1,86 @@
-import React, { Component } from 'react';
-import SushiContainer from './containers/SushiContainer';
-import Table from './containers/Table';
+import React, { Component } from "react";
+import SushiContainer from "./containers/SushiContainer";
+import Table from "./containers/Table";
 
 // Endpoint!
-const API = "http://localhost:3000/sushis"
+const API = "http://localhost:5000/sushis";
 
 class App extends Component {
+  // 2. after geting the data set state to an empty array
+  state = {
+    allMySushis: [],
+    startIndex: 0,
+    eatenSushi: [],
+    wallet: 1000
+    //37 adding wallet as state starting with $1000
+    //25 setting a state to include an empty
+    //      array that can include eaten sushi
+    // 14. setting state to include indexes for splicing
+  };
+
+  // 1. get all the data from an endpoint
+  componentDidMount() {
+    fetch(API)
+      .then((resp) => resp.json())
+      .then((sushiObjects) => {
+        this.setState({ allMySushis: sushiObjects });
+      });
+  }
+  //3. set state to the sushi objects
+
+  //17. Updating state to include more sushis with a function
+  moreSushiButton = () => {
+    this.setState({
+      startIndex: this.state.startIndex + 4,
+    });
+  };
+
+  //35. passing in sushiObjects as argument so you can access them and 
+  //    pass them down as the thing that goes into the array
+  addPlate = (sushiObjects) => {
+    this.setState({
+      eatenSushi: [...this.state.eatenSushi, sushiObjects.props.sushi],//35. adding .props.sushi to just return the single object when clicking
+    }); //29. function to update state
+  };
+
+  //40. buying sushi function to manipulate wallet
+  buySushi = (cost) =>{
+    this.setState ({
+      wallet: this.state.wallet - cost
+    })
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer
+          startIndex={this.state.startIndex}
+          allMySushis={this.state.allMySushis}
+          moreSushiButton={this.moreSushiButton}
+          addPlate={this.addPlate}
+          buySushi={this.buySushi}
+          wallet={this.state.wallet}
+
+        />
+        {/* 5. Passing Down Props into child component*/}
+        {/* 16. passing down index props */}
+        {/* 18. passing down the function through props */}
+        {/* 30. passing the function to sushi  container*/}
+        {/* 41. passing the prop call back function buySushi to sushi container */}
+        {/* 44. adding wallet to Sushi Container */}
+        <Table eatenSushi={this.state.eatenSushi} 
+                wallet={this.state.wallet}
+        />
+        {/* 27. passing down eatenShush as props to container */}
+        {/* 38. passing wallet down to table child */}
+        
       </div>
     );
   }
 }
 
 export default App;
+// 4. now think about rendering sushis
+//     and rendering them in the sushi container
+
+//26. passing down eatnSushi down to table.
--- sushi-saga-client/src/components/Sushi.js
+++ sushi-saga-client/src/components/Sushi.js
@@ -1,23 +1,51 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, Component } from 'react'
 
-const Sushi = (props) => {
+class Sushi extends Component {
+  state={
+    eaten: false
+  }
+
+
+// 24. defining the prop call back function here setting state to true
+eatingSushiClick = () => {
+  if (this.props.wallet >= this.props.sushi.price) {
+//46. adding logic to not go negative
+    this.setState({
+      eaten: true
+    })
+    this.props.addPlate(this)//34. adding this as an argument which is suhiObjects
+    {/*  32. callin on a prop call back function to fire on click*/}
+    this.props.buySushi(this.props.sushi.price)
+    //43. calling on prop call back function on click
+  }
+}
+
+//21. Changing the state of Sushi so that 
+//      it can have state we can manipulate
+  render(){
   return (
     <div className="sushi">
       <div className="plate" 
-           onClick={/* Give me a callback! */ null}>
-        { 
-          /* Tell me if this sushi has been eaten! */ 
-          false ?
-            null
+           onClick={this.state.eaten ? null : this.eatingSushiClick }>
+           {/* 23. making a prop call back fuction  to fire on click*/}
+           {/* 22. ternary shecking state if not eaten => show picture */}
+           {/* 33. adding logic to only add one plate */}
+           
+        { this.state.eaten
+          ? null
           :
-            <img src={/* Give me an image source! */ } width="100%" />
+            <img src={this.props.sushi.img_url} width="100%" />
         }
       </div>
       <h4 className="sushi-details">
-        {/* Give me a name! */} - ${/* Give me a price! */}
+        {this.props.sushi.name} - ${this.props.sushi.price}
+        {/* 11. graabbing name and price and img_url from props passed down */}
       </h4>
     </div>
   )
 }
-
+}
 export default Sushi
+
+//9. Have to pass the props down first so back to the parent Container
+//12. now lets think about limiting
